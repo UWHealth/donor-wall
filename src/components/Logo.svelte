eb2afa99ea03b4f883d0661475d59420f313581e
da-uwhealth <damiott@uwhealth.org>
2025-04-08 13:02:44 -0500

    add logo and logo component

---
+++ src/components/Logo.svelte
@@ -0,0 +1,20 @@
+<script>
+    import logoImage from '$assets/uw-transplant-logo.png';
+    let classes = '';
+  
+    export {classes as class};
+    export let logo = logoImage;
+  </script>
+
+  <div class={classes}>
+    <img class="image" src={logo} alt="UWH Transplant Logo" />
+  </div>
+  
+
+  
+  <style lang="scss">
+   .image {
+    animation: fade-grow-in-fast 25s 1 both normal ease-out;
+   }
+  </style>
+  
